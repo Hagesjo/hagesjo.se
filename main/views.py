68fbc0882f934fde5cadc33f1ce0962a9512d4a5
Andreas Hagesjö <andreas.hagesjo@gmail.com>
2016-02-16 20:34:31 +0100

    Cleanup

--- main/views.py
+++ main/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-	return render(request, 'main/base.html')
-
 def about(request):
 	return render(request, 'main/about.html')
+
+def links(request):
+	return render(request, 'main/links.html')
