5a484fc7e3c8da72056d25e5f279b961ba35293c
Andreas Hagesjö <andreas.hagesjo@gmail.com>
2016-02-16 20:59:58 +0100

    Updated content

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'^about/', views.about, name='about'),
+	url(r'^$', views.about, name='about'),
+	url(r'^links/', views.links, name='links'),
 ]
